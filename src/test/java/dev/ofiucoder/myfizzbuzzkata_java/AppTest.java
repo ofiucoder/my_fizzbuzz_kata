f5807acaf06a14affcf4c7cfd028514bec736b36
Alberto Casas <alberto.casas.ofiuco@gmail.com>
2024-10-30 21:42:04 +0100

    feat: pom.xml file

---
+++ src/test/java/dev/ofiucoder/myfizzbuzzkata_java/AppTest.java
@@ -0,0 +1,18 @@
+package dev.ofiucoder.myfizzbuzzkata_java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
